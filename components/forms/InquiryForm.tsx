17a650c8afb6a0420ce83e5243835a342af906b1
shagaofeng-rgb <shagaofeng@gmail.com>
2026-08-23 22:11:46 +0800

    Fix analytics header import in inquiry form

--- components/forms/InquiryForm.tsx
+++ components/forms/InquiryForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { getAnalyticsRequestHeaders } from "@/components/analytics/AnalyticsTracker";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
