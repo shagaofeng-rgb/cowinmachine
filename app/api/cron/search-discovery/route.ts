260ad1cf1498a26fcbea5e74f1a9b00347141482
shagaofeng-rgb <shagaofeng@gmail.com>
2026-09-07 13:58:09 +0800

    Correct Google OAuth JWT grant type

--- app/api/cron/search-discovery/route.ts
+++ app/api/cron/search-discovery/route.ts
@@ -32,7 +32,7 @@ async function getGoogleAccessToken(serviceAccount: GoogleServiceAccount) {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams({
-      grant_type: "urn:ietf:params:oauth-bearer",
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
       assertion,
     }),
   });
